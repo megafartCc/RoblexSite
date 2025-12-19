e37a369c9979fbcdb824ada38cfd1de509f58f91
Roblex User <roblex@example.com>
2025-12-18 20:42:56 -0600

    Add blank landing page with black background

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./screens/LoginPage";
+import { LandingPage } from "./screens/LandingPage";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/admin/login" replace />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/admin/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/admin/login" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
