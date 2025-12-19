c4dbb9fc976a5096f5644ad03861bcf345d7d030
Roblex User <roblex@example.com>
2025-12-18 20:40:52 -0600

    Remove landing page and revert to admin login only

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./screens/LoginPage";
-import { LandingPage } from "./screens/LandingPage";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<Navigate to="/admin/login" replace />} />
         <Route path="/admin/login" element={<LoginPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/admin/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
